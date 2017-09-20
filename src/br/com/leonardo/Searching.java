2b2aab62efdf2c97b0702c6b0375e0ceff8539c2
Leonardo Matos Pessoa <leonardom.pessoa@gmail.com>
2017-09-19 22:45:02 -0300

    Adição das primeiras classes

--- src/br/com/leonardo/Searching.java
+++ src/br/com/leonardo/Searching.java
@@ -5,6 +5,7 @@ public class Searching {
     public static void main(String[] args) {
 
         char [] array = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
         System.out.println("linear search: " + linearSearch(array, 'h'));
         System.out.println("linear search: " + linearSearch(array, '§'));
         System.out.println("Sentinel linear search: " + sentinelLinearSearch(array, 'h'));
