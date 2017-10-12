1620a9e6ece471c6ed0cc298ac44fed91981a331
Leonardo Matos Pessoa <leonardom.pessoa@gmail.com>
2017-10-12 00:17:10 -0300

    Coding Studies

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,42 +1,49 @@
 import java.util.Iterator;
 
-public class LinkedListQueue<T> implements Queue<T> {
+public class ArrayQueue<T> implements Queue<T> {
 
-    private Node first;
-    private Node last;
+    private T[] queue;
+    private int head, tail;
 
-    private class Node {
-        T item;
-        Node next;
+    public ArrayQueue() {
+        queue = (T[]) new Object[1];
     }
 
     @Override
     public void enqueue(T item) {
-        Node oldlast = last;
-        last = new Node();
-        last.item = item;
-        last.next = null;
-        if(isEmpty()) {
-            first = last;
-        } else {
-            oldlast.next = last;
+        queue[tail++] = item;
+        if (queue.length == tail) {
+            resize(2 * queue.length);
         }
-
     }
 
     @Override
     public T dequeue() {
-        T item = first.item;
-        first = first.next;
-        if(isEmpty()) {
-            last = null;
+        T item = queue[head];
+        queue[head] = null;
+        head++;
+        if (head % tail == tail / 2) {
+            resize((queue.length / 2));
         }
         return item;
     }
 
+
+    private void resize(int capacity) {
+        T[] copyArray = (T[]) new Object[capacity];
+        head = 0;
+        tail = 0;
+        for (int i = 0; i < queue.length; i++) {
+            if (queue[i] != null) {
+                copyArray[tail++] = queue[i];
+            }
+        }
+        queue = copyArray;
+    }
+
     @Override
     public boolean isEmpty() {
-        return first == null;
+        return  queue.length == 0|| queue[head] == null;
     }
 
     @Override
@@ -46,23 +53,22 @@ public class LinkedListQueue<T> implements Queue<T> {
 
     private class QueueIterator implements Iterator<T> {
 
-        Node current = first;
+        int current = head;
 
         @Override
         public boolean hasNext() {
-            return current != null;
+            return queue[current] != null;
         }
 
         @Override
         public T next() {
-            T item = current.item;
-            current = current.next;
+            T item = queue[current++];
             return item;
         }
     }
 
     public static void main(String[] args) {
-        Queue<Integer> ints = new LinkedListQueue<>();
+        Queue<Integer> ints = new ArrayQueue<>();
         ints.enqueue(1);
         ints.enqueue(2);
         ints.enqueue(3);
@@ -82,4 +88,5 @@ public class LinkedListQueue<T> implements Queue<T> {
         }
 
     }
+
 }
